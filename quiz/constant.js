074740e534f3b5a028e0e18ad664a782911e14fa
EvaTolopilo <21it2.tolopilo.e@pdu.by>
2024-07-02 15:39:02 +0300

    Add files via upload

---
+++ quiz/constant.js
@@ -0,0 +1,8 @@
+export const start_btn = document.getElementById("start_btn")
+export const quiz_box = document.getElementById("quiz_box")
+export const result_box = document.getElementById("result_box")
+export const restart_btn = result_box.getElementById("restart_btn")
+export const option_list = document.getElementById("option_list")
+export const timeText = document.getElementById("time_left")
+export const timeCount = document.getElementById("timer_sec")
+export const theme_btn = document.getElementById("theme_btn")
---
+++ quiz/constants.js
@@ -0,0 +1,8 @@
+export const start_btn = document.querySelector(".start_btn button");
+export const quiz_box = document.querySelector(".quiz_box");
+export const result_box = document.querySelector(".result_box");
+export const restart_btn = result_box.querySelector(".buttons .restart");
+export const option_list = document.querySelector(".option_list");
+export const timeText = document.querySelector(".timer .time_left_txt");
+export const timeCount = document.querySelector(".timer .timer_sec");
+export const theme_btn = document.querySelector(".theme_btn");
