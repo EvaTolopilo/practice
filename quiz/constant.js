836b5e9a8e204bbbef06eff7464d7893855ae089
EvaTolopilo <21it2.tolopilo.e@pdu.by>
2024-07-02 15:38:29 +0300

    Delete quiz directory

--- quiz/constant.js
+++
@@ -1,7 +0,0 @@
-const start_btn = document.querySelector(".start_btn button");
-const quiz_box = document.querySelector(".quiz_box");
-const result_box = document.querySelector(".result_box");
-const restart_btn = result_box.querySelector(".buttons .restart");
-const option_list = document.querySelector(".option_list");
-const timeText = document.querySelector(".timer .time_left_txt");
-const timeCount = document.querySelector(".timer .timer_sec");
